e5525f668b2acfc168a2c02d0b9f73207c225a6f
RITIK SINGH <111556793+Ritik10x@users.noreply.github.com>
2026-04-17 02:07:21 +0530

    code update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,9 +2,24 @@ const express = require('express')
 
 const app = express();
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from our Server")
-});
+
+app.get('/user',(req,res)=>{
+    console.log(req.query)
+    res.send({
+        FirstName: "Ritik", Latname:"singh",
+    })
+})
+
+app.get('/user/:userId/:Name/:Password',(req,res)=>{
+    console.log(req.params)
+    res.send({
+        FirstName: "Ritik", Latname:"singh",
+    })
+})
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from our Server")
+// });
 
 app.listen(7777,()=>{
     console.log("Server is sucessfully Listening on port 7777")
